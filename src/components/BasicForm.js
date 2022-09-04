796e69b55985a6309940d435a5ff889016f8766e
tasneem1998 <tasneem.k@homesfy.in>
2022-09-04 19:37:20 +0530

    1st commit - basic form setup

---
+++ src/components/BasicForm.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const BasicForm = () => {
+    return (
+        <>
+            <Container maxWidth="md" sx={{ my: 4 }}>
+                <Typography variant="h1">
+                    Basic Form here....
+                </Typography>
+            </Container>
+        </>
+    )
+}
+
+export default BasicForm
